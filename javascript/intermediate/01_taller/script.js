aa8055861922898052984b565ca6237c6dae45c2
alvercajose <alvercacjose@gmail.com>
2024-06-29 12:29:37 -0500

    wa
    wa

--- javascript/intermediate/01_taller/script.js
+++ javascript/intermediate/01_taller/script.js
@@ -3,24 +3,25 @@ var texto = "Programacion y Plataformas Web"
 
 function imprimir_01() {
     let texto2 = "Avengers"
-    //Esto no se debe hacer
+    // Esto no se debe hacer.
     var texto3 = "Liga de la Justicia"
 
-    console.log(texto)
-    console.log(texto2)
-    console.log(texto3)
-    
-    //No se puede cmabiar a una constante.
-    ULR = "http://www.ups.edu.ec/home"
+    console.log( texto )
+    console.log( texto2 )
+    console.log( texto3 )
+
+    // No se puede cambiar a una constante.
+    URL = "http://www.ups.edu.ec/home"
+
     console.log(URL)
 }
 
 function imprimir_02() {
-    console.log(texto)
-    // texto 2 no existe en esta funcion
-    console.log(texto2)
-    // texto 3 no existe en esta funcion
-    console.log(texto3)
+    console.log( texto )
+    // texto2 no existe en esta funcion.
+    console.log( texto2 )
+    // texto3 no existe en esta funcion.
+    console.log( texto3 )
 }
 
 imprimir_01()
