eb68ef66edf554ebfc5c3ba4ebc5b0f5117a6a7b
alvercajose <alvercacjose@gmail.com>
2024-07-20 12:40:37 -0500

    json

---
+++ javascript/advanced/ejercicio_01/script.js
@@ -0,0 +1,22 @@
+const URL = 'https://rickandmortyapi.com/api/character/'
+var xhttp = null
+
+function conectar (personaje){
+    xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = obtener_respuesta
+    xhttp.open('GET', URL + personaje)
+    xhttp.send()
+}
+
+function obtener_respuesta(){
+    if(xhttp != null && xhttp.onreadystate == 4){
+        if(xhttp.status == 200){
+            let dato = JSON.parse(xhttp.responseText)
+            
+            let container = document.getElementById('container')
+            let personaje = `<h1>${dato.name}</h1>
+            <img src="${dato.image}"/>`
+            container.innerHTML = personaje
+        }
+    }
+}
