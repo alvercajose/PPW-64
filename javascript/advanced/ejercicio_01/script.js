f1d72d6c02e0f7f87e5dcaadc99d03f3b178ec90
alvercajose <alvercacjose@gmail.com>
2024-08-03 11:04:54 -0500

    omaigaaahhhh
    al dia en todo omg

--- javascript/advanced/ejercicio_01/script.js
+++ javascript/advanced/ejercicio_01/script.js
@@ -1,21 +1,22 @@
 const URL = 'https://rickandmortyapi.com/api/character/'
 var xhttp = null
 
-function conectar (personaje){
+function conectar(personaje) {
     xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = obtener_respuesta
-    xhttp.open('GET', URL + personaje)
+    xhttp.open('GET', URL + personaje, true)
     xhttp.send()
 }
 
-function obtener_respuesta(){
-    if(xhttp != null && xhttp.onreadystate == 4){
-        if(xhttp.status == 200){
-            let dato = JSON.parse(xhttp.responseText)
-            
+function obtener_respuesta() {
+    if (xhttp != null && xhttp.readyState == 4) {
+        if (xhttp.status == 200) {
+            let dato =  JSON.parse(xhttp.responseText)
+            console.log(dato)
+
             let container = document.getElementById('container')
-            let personaje = `<h1>${dato.name}</h1>
-            <img src="${dato.image}"/>`
+            let personaje = `<div class="container-item"><h1>${dato.name}</h1>
+            <img src="${dato.image}" /><div class="container-item">`
             container.innerHTML = personaje
         }
     }
