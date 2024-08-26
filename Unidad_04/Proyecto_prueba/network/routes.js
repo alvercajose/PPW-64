f915419e0a7c35975c67e9b2d51bd0a171a98032
alvercajose <alvercacjose@gmail.com>
2024-08-26 02:42:36 -0500

    route

--- Unidad_04/Proyecto_prueba/network/routes.js
+++ Unidad_04/Proyecto_prueba/network/routes.js
@@ -1,7 +1,4 @@
 
-const usuario = require('../components/usuario/interface')
-const ciudad = require('../components/ciudad/interface')
-const pais = require('../components/pais/interface')
 
 const routes = function( server ) {
     server.use('/usuario', usuario)
